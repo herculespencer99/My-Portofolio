ef7b776fc12f67730b066b8b417b4e7a06444f7f
Hércules <herculespencer99>
2023-08-19 22:12:09 -0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#intro',{
+    duration: 3000,
+    rotate:{x: 30, y: 30, z: 0}}
+    );
+
+sr.reveal('#aboutme',{
+    duration: 3000,
+    rotate:{x: 30, y: 30, z: 0}}
+    );
+
+sr.reveal('#featured',{
+    duration: 3000,
+    rotate:{x: 30, y: 30, z: 0}}
+    );
