715215de064dde8a503fde2159205076d7d58367
Hércules <herculespencer99>
2023-09-13 21:44:18 -0100

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 window.sr = ScrollReveal({reset: true});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('ul li a');
+
+console.log(navLinks);
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+
+
+        if (top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('ul li a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
 
 sr.reveal('#intro',{
     duration: 3000,
