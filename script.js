4ee8cc6bd9dc1f8b9168219adbde5ff71d206dd2
Hércules <herculespencer99>
2023-09-15 19:35:12 -0100

    Second Commit

--- script.js
+++ script.js
@@ -16,16 +16,26 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('ul li a [href*=' + id + ']').classList.add('active');
             });
+            let correspondingLink = document.querySelector('ul li a[href="#' + id + '"]');
+            if (correspondingLink) {
+                correspondingLink.classList.add('active');
         };
-    });
-};
+    }
+});
+} 
 
-sr.reveal('#intro',{
+/* sr.reveal('#intro',{
     duration: 3000,
     rotate:{x: 30, y: 30, z: 0}}
-    );
+    ); */
+sr.reveal('.description',{distance: '-100px',
+    duration:3000}
+)
+sr.reveal('#intro img',{delay: 700,
+    distance:'70px',
+    duration: 3000}
+)
 
 sr.reveal('#aboutme',{
     duration: 3000,
@@ -36,3 +46,35 @@ sr.reveal('#featured',{
     duration: 3000,
     rotate:{x: 30, y: 30, z: 0}}
     );
+
+sr.reveal('#contact',{
+    duration: 3000,
+    rotate:{x: 30, y: 30, z: 0}}
+    );
+
+/* Add Blur to header */
+ const blurNavbar = () =>{
+     
+     const navbar = document.querySelector('.navbar');
+    this.scrollY >= 50 ? navbar.classList.remove('navbar-blur') 
+                        :navbar.classList.add('navbar-blur') 
+                       
+};
+ window.addEventListener('scroll', blurNavbar)
+
+ /*===============================SCROLL UP ========================= */
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') 
+                        : scrollUp.classList.remove('show-scroll') 
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/* =============================== CHANGE THEME ====================================== */
+const changeThemeBtn = document.querySelector('#change-theme')
+
+changeThemeBtn.addEventListener("change",()=>{
+    document.body.classList.toggle('dark')
+});
